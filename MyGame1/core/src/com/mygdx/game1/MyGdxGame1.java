d7e86a35eeb0ddafb004c0a1779ac313a2b20be8
v02qabs <takesue090@gmail.com>
2019-11-16 23:09:49 +0900

    batch-image1

--- MyGame1/core/src/com/mygdx/game1/MyGdxGame1.java
+++ MyGame1/core/src/com/mygdx/game1/MyGdxGame1.java
@@ -20,9 +20,7 @@ public class MyGdxGame1 extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
